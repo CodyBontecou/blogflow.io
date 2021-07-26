92c5822c1b279c6a9c9e21c6261138d081103259
Cody Bontecou <bontecouc@gmail.com>
2021-07-25 23:28:00 -1000

    Automated posts to all included services.

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { grayMatter } from './mdParser'
 import { createDevToPost } from './devto/index'
 import { createMediumPost } from './medium/index'
 import { createHashnodePost } from './hashnode/index'
+import { postLink, url, title, subreddits } from './reddit/index'
 
 axios.defaults.headers.common['content-type'] = 'application/json'
 
-// createDevToPost(grayMatter)
+createDevToPost(grayMatter)
 createMediumPost(grayMatter)
-// createHashnodePost(grayMatter)
+createHashnodePost(grayMatter)
+
+subreddits.forEach(subreddit => postLink(title, url, subreddit))
